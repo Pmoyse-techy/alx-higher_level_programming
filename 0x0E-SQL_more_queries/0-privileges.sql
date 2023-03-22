1b89837c2bc0d185268fde02974648aab7f25e21
Pmoyse-techy <promisemoyse10@gmail.com>
2023-03-22 07:47:29 -0700

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- Script that lists all privileges of the MySQL users
--- Query to list all privileges (GRANT) of the MySQL users
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
